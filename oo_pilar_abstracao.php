888b74bfabfcd68701d5d00fe28f9dc511e6cf38
Neandro <neandrosilva.11@gmail.com>
2023-06-24 21:38:02 -0300

    chamando métodos internamente

--- oo_pilar_abstracao.php
+++ oo_pilar_abstracao.php
@@ -43,10 +43,10 @@
 
         //métodos
         function resumirCadFunc() {
-            if($this->numFilho) {
-                return "$this->nome, possui $this->numFilho filho(s)";
+            if($this->__get('numFilho')) {
+                return $this->__get('nome') . ', possui ' . $this->__get('numFilho') . ' filho(s)';
             }else {
-                return "$this->nome, não possui $this->numFilho filho(s)";
+                return $this->__get('nome') . ', não possui ' . $this->__get('numFilho') . ' filho(s)';
             }
             
         }
@@ -60,18 +60,19 @@
     $y = new Funcionario();
 
     $y->__set('nome', 'José');
-    $y->__set('numFilhos', 2);
+    $y->__set('numFilho', 2);
     $y->__set('telefone', '749999-9999');
     $y->__set('cargo', 'Programador PHP');
     $y->__set('salario', '4.500,00');
-    echo $y->__get('nome') . ' possue ' . $y->__get('numFilhos') . ' filho(s), o número de telefone é ' . $y->__get('telefone') . ', trabalha como ' . $y->__get('cargo') . ', com salario de ' . $y->__get('salario');
-    echo '<br>';
+   // echo $y->__get('nome') . ' possue ' . $y->__get('numFilhos') . ' filho(s), o número de telefone é ' . $y->__get('telefone') . ', trabalha como ' . $y->__get('cargo') . ', com salario de ' . $y->__get('salario');
+   echo $y->resumirCadFunc(); 
+   echo '<br>';
     $x = new Funcionario();
     $x->__set('nome', 'Maria');
     $x->__set('numFilhos', 0);
     $x->__set('telefone', '749991-9191');
     $x->__set('cargo', 'Programadora Python');
     $x->__set('salario', '5.000,00');
-    echo $x->__get('nome') . ' possue ' . $x->__get('numFilhos') . ' filho(s), o número de telefone é ' . $x->__get('telefone') . ', trabalha como ' . $x->__get('cargo') . ', com salario de ' . $x->__get('salario');
- 
+   // echo $x->__get('nome') . ' possue ' . $x->__get('numFilhos') . ' filho(s), o número de telefone é ' . $x->__get('telefone') . ', trabalha como ' . $x->__get('cargo') . ', com salario de ' . $x->__get('salario');
+   echo $x->resumirCadFunc();
 ?>
