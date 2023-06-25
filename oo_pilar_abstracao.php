08bf83a71396730a1ff8d657c9cdbe19cbc463e9
Neandro <neandrosilva.11@gmail.com>
2023-06-24 21:17:56 -0300

    Getts e setters mágicos (overloading de atributos e...

--- oo_pilar_abstracao.php
+++ oo_pilar_abstracao.php
@@ -5,8 +5,18 @@
         public $nome = null;
         public $telefone = null;
         public $numFilho = null;
+        public $cargo = null;
+        public $salario = null;
 
-        //getters setters
+        //getters setters (overloading / sobrecargar)
+        function __set($atributo, $valor) {
+            $this->$atributo = $valor;
+        }
+
+        function __get($atributo) {
+            return $this->$atributo;
+        }
+        /*
         function setNome($nome) {
             $this->nome = $nome;
         }
@@ -29,7 +39,7 @@
 
         function getNumFilhos() {
             return $this->numFilho;
-        }
+        }*/
 
         //métodos
         function resumirCadFunc() {
@@ -48,9 +58,20 @@
     }
 
     $y = new Funcionario();
-    $y->setNome('José');
-    $y->setNumFilhos(2);
-    echo $y->resumirCadFunc();
+
+    $y->__set('nome', 'José');
+    $y->__set('numFilhos', 2);
+    $y->__set('telefone', '749999-9999');
+    $y->__set('cargo', 'Programador PHP');
+    $y->__set('salario', '4.500,00');
+    echo $y->__get('nome') . ' possue ' . $y->__get('numFilhos') . ' filho(s), o número de telefone é ' . $y->__get('telefone') . ', trabalha como ' . $y->__get('cargo') . ', com salario de ' . $y->__get('salario');
+    echo '<br>';
     $x = new Funcionario();
+    $x->__set('nome', 'Maria');
+    $x->__set('numFilhos', 0);
+    $x->__set('telefone', '749991-9191');
+    $x->__set('cargo', 'Programadora Python');
+    $x->__set('salario', '5.000,00');
+    echo $x->__get('nome') . ' possue ' . $x->__get('numFilhos') . ' filho(s), o número de telefone é ' . $x->__get('telefone') . ', trabalha como ' . $x->__get('cargo') . ', com salario de ' . $x->__get('salario');
  
 ?>
