6d7607ad85c3faa2b3b685f98c9bb6644b6bc4c1
Neandro <neandrosilva.11@gmail.com>
2023-07-03 18:12:28 -0300

    pilar do encapsulamento

---
+++ oo_pilar_encapsulamento.php
@@ -0,0 +1,44 @@
+<?php
+
+    class Pai {
+        private $nome = 'Neandro';
+        protected $sobrenome = 'Silva';
+        public $humor = 'Feliz';
+
+        public function __get($attr) {
+            return $this->$attr;
+        }
+
+        public function __set($attr, $value) {
+            $this->$attr = $value;
+        }
+
+        private function executarMania() {
+            echo 'Assobiar';
+        }
+
+        protected function responder() {
+            echo 'Oi';
+        }
+
+        public function executarAcao() {
+            $x = rand(1, 10);
+
+            if($x >= 1 && $x <=8) {
+                $this->responder();
+            } else {
+                $this->executarMania();
+            }
+        }
+
+      
+    }
+
+    $pai = new Pai();
+    //echo $pai->humor; 
+    echo $pai->sobrenome;
+    echo '<br>';
+    echo $pai->sobrenome = 'Da Silva';
+    echo '<br>';
+    $pai->executarAcao();
+?>
